d58be05fc0d1cc081e79c1c2c797276aeedefcac
RaymondXue <raymond@0587352705.wireless.umich.net>
2018-11-28 18:02:00 -0500

    Add other headlines

--- top_headlines.py
+++ top_headlines.py
@@ -4,6 +4,13 @@ from secrets_example import *
 
 app = Flask(__name__)
 
+def get_headline(section):
+	baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+	extendedurl = baseurl + section + '.json'
+	params = { 'api-key':api_key }
+	results = requests.get(extendedurl, params).json()['results']
+
+	return results
 
 @app.route('/')
 def index():	
@@ -11,15 +18,19 @@ def index():
 
 @app.route('/user/<nm>')
 def hello_name(nm):
-	baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
-	section = 'world'
-	extendedurl = baseurl + section + '.json'
-	params = { 'api-key':api_key }
-	results = requests.get(extendedurl, params).json()['results']
+	results = get_headline('technology')
 	headlines = []
 	for r in results:
 		headlines.append(r['title']+' ('+r['url']+')')
 	return render_template('user.html', name=nm, my_list=headlines[:5])
+
+@app.route('/user/<nm>/<section>')
+def show_headline(nm, section):
+	results = get_headline(section)
+	headlines = []
+	for r in results:
+		headlines.append(r['title']+' ('+r['url']+')')
+	return render_template('headline.html', name=nm, section=section, my_list=headlines[:5])
 
 if __name__ == '__main__':
 	app.run(debug=True)
