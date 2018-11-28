a57ee63fdf6b102eda61a3748dbb6673cbc9737c
RaymondXue <raymond@RaymondXues-MacBook-Pro.local>
2018-11-28 14:29:29 -0500

    Add technology headlines on user page

--- top_headlines.py
+++ top_headlines.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+import requests
+from secrets_example import *
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():	
+	return '<h1>Welcome!</h1>'
+
 @app.route('/user/<nm>')
 def hello_name(nm):
-    return render_template('user.html', name=nm)
+	baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+	section = 'world'
+	extendedurl = baseurl + section + '.json'
+	params = { 'api-key':api_key }
+	results = requests.get(extendedurl, params).json()['results']
+	headlines = []
+	for r in results:
+		headlines.append(r['title']+' ('+r['url']+')')
+	return render_template('user.html', name=nm, my_list=headlines[:5])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
